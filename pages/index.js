d8fb14d0e564b874de00cbc543a484d33286a45b
David Bergeron <david.f.bergeron@gmail.com>
2020-04-10 15:22:47 -0600

    Tweaks and changes

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,65 @@
-import Head from 'next/head'
+import { Layout } from "../components/Layout";
+import { useThemeContext } from "../components/ThemeContext";
 
-import { Layout } from '../components/Layout'
-import GlitchCanvas from '../components/GlitchCanvas'
-import { useThemeContext } from '../components/ThemeContext'
-
-const Home = (props) => {
-  const [theme] = useThemeContext()
+const Home = () => {
+  const [theme] = useThemeContext();
   return (
     <Layout>
-      {/* <GlitchCanvas /> */}
-
       <div>
         <h1>I'm David.</h1>
         <p>Welcome.</p>
       </div>
 
       <h2>
-        I'm a developer, <span className='accent'>designer</span>, and creation
+        I'm a developer, <span className="accent">designer</span>, and creation
         enthusiast.
       </h2>
 
-      <p>I make websites and apps with React.</p>
+      <p>
+        I make websites and apps using modern tools like
+        <a href="https://nextjs.org/"> Next.JS</a>,
+        <a href="https://gatsbyjs.org">Gatsby</a> and
+        <a href="https://reactnative.dev"> React Native</a>.
+      </p>
 
-      <p>I'm currently living in the beautiful city of Edmonton, Alberta.</p>
+      <p>Currently living in the beautiful city of Edmonton, Alberta.</p>
 
       <p>
-        Get in touch with me on
-        <a href='https://twitter.com/DavidFBergeron'> Twitter</a> or hit me up
-        by <a href='mailto: david.f.bergeron@gmail.com'>e-mail.</a>
+        <a id="mail-me" href="mailto: david.f.bergeron@gmail.com">
+          Let's chat.
+        </a>
       </p>
-      {/* <GlitchCanvas /> */}
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
         h1 {
           padding-top: 20vh;
           display: flex;
           align-items: baseline;
         }
-        p span {
-          font-weight: 700;
+        span {
+          font-weight: 500;
           font-size: 1.2rem;
         }
-        span {
-          font-weight: 400;
-          font-size: 0.9rem;
+
+        a {
+          font-weight: 500;
+          color: gray;
+        }
+
+        #mail-me,
+        #mail-me:visited {
+          color: rgb(255, 134, 44);
+          padding: 1rem 1rem;
+          font-weight: 500;
+          transition: all 0.2s ease-in-out;
+        }
+
+        #mail-me:hover {
+          color: #fff;
+          background: rgb(255, 134, 44);
         }
       `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
